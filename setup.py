86ad01964b770ee1c3fbcd6e76c984424e10da74
ndsndatascientist <127064232+ndsndatascientist@users.noreply.github.com>
2023-04-13 20:27:55 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bot de Telegram voltado para Análise de Dados fictícios de RH, aliado a um projeto gravado em vídeo para o canal "Iuri Rosal - Resumo de Sucesso"',
+    author='Newton Nascimento',
+    license='',
+)
